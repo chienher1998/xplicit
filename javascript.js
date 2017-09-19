4cf5c8ed6e5cf832a4362fc2a02ed310a7776171
chienher1998 <31813377+chienher1998@users.noreply.github.com>
2017-09-19 10:38:46 +0800

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,18 @@
+﻿(function() {
+
+	var preload=document.getElementById("preload");
+	var loading= 0;
+	var id = setInterval(frame, 64);
+	
+	function frame() {
+		if(loading ==50) {
+			clearInterval(id);
+			window.open("Explicit home page.html", "_self");
+			} else {
+			loading = loading + 1;
+			if(loading == 30) {
+				preload.style.animation= "fadeout 2s ease";
+				}
+			}
+		}
+	})();
